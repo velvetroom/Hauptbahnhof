73eebb27d425b0ae1895dc31a1c1b44fb9e8707d
vauxhall <zero.griffin@gmail.com>
2018-11-01 10:39:44 +0100

    Show caret in text view

--- Application/Game/GamePresenter.swift
+++ Application/Game/GamePresenter.swift
@@ -1,7 +1,19 @@
 import CleanArchitecture
+import MarkdownHero
 
 class GamePresenter:Presenter {
+    private let hero = Hero()
+    
+    required init() {
+        hero.font = .systemFont(ofSize:20, weight:.light)
+    }
+    
     @objc func home() {
         Application.navigation.setViewControllers([HomeView()], animated:true)
+    }
+    
+    override func didAppear() {
+        hero.parse(string:"Hello world") { [weak self] message in self?.update(viewModel:message) }
+        
     }
 }
--- Application/Game/GameView.swift
+++ Application/Game/GameView.swift
@@ -2,11 +2,16 @@ import CleanArchitecture
 
 class GameView:View<GamePresenter> {
     private weak var text:UITextView!
+    private weak var caret:UIView!
+    private weak var caretLeft:NSLayoutConstraint!
+    private weak var caretTop:NSLayoutConstraint!
+    private weak var caretHeight:NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         makeOutlets()
+        viewModel()
     }
     
     private func makeOutlets() {
@@ -21,15 +26,18 @@ class GameView:View<GamePresenter> {
         let text = UITextView()
         text.translatesAutoresizingMaskIntoConstraints = false
         text.backgroundColor = .clear
-        text.textColor = .white
         text.indicatorStyle = .white
-        text.font = .systemFont(ofSize:20, weight:.light)
-        text.contentInset = .zero
         text.textContainerInset = UIEdgeInsets(top:10, left:15, bottom:20, right:15)
-        text.text = "Hello world"
         text.isEditable = false
         view.addSubview(text)
         self.text = text
+        
+        let caret = UIView()
+        caret.translatesAutoresizingMaskIntoConstraints = false
+        caret.isUserInteractionEnabled = false
+        caret.backgroundColor = .spreeBlue
+        view.addSubview(caret)
+        self.caret = caret
         
         bar.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
         bar.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
@@ -38,6 +46,14 @@ class GameView:View<GamePresenter> {
         text.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
         text.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
         
+        caretTop = caret.topAnchor.constraint(equalTo:text.topAnchor)
+        caretLeft = caret.leftAnchor.constraint(equalTo:text.leftAnchor)
+        caretHeight = caret.heightAnchor.constraint(equalToConstant:0)
+        caret.widthAnchor.constraint(equalToConstant:8).isActive = true
+        caretTop.isActive = true
+        caretLeft.isActive = true
+        caretHeight.isActive = true
+        
         text.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
         if #available(iOS 11.0, *) {
             text.contentInsetAdjustmentBehavior = .never
@@ -45,5 +61,18 @@ class GameView:View<GamePresenter> {
         } else {
             bar.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
         }
+    }
+    
+    private func viewModel() {
+        presenter.viewModel { [weak self] (message:NSAttributedString) in self?.update(message:message) }
+    }
+    
+    private func update(message:NSAttributedString) {
+        text.attributedText = message
+        text.textColor = .white
+        let rect = text.caretRect(for:text.endOfDocument)
+        caretTop.constant = rect.minY
+        caretLeft.constant = rect.minX + 5
+        caretHeight.constant = rect.height
     }
 }
