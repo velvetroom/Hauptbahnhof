2b039e34c857235c6f1a748dca269c40f0f5dbcd
vauxhall <zero.griffin@gmail.com>
2018-11-13 08:42:54 +0100

    Add player stats

--- Source/Entities/Player.swift
+++ Source/Entities/Player.swift
@@ -3,4 +3,9 @@ import Foundation
 public class Player:Codable {
     var chapter = Chapter.One
     var state = "initial"
+    var courage = 0
+    var knowledge = 0
+    var dilligence = 0
+    var empathty = 0
+    var score = 0
 }
