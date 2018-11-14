43ff00b6f9d3ffa78b1668be9ddf260e0862789f
vauxhall <zero.griffin@gmail.com>
2018-11-14 12:21:38 +0100

    Add splash screen

--- Application/Shared/SplashView.swift
+++ Application/Shared/SplashView.swift
@@ -3,12 +3,17 @@ import Hauptbahnhof
 
 class SplashView:UIViewController {
     override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .black
-        loadResources()
+        let logo = UIImageView(image:#imageLiteral(resourceName: "splash.pdf"))
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.clipsToBounds = true
+        logo.contentMode = .center
+        logo.isUserInteractionEnabled = false
+        view.addSubview(logo)
+        logo.centerXAnchor.constraint(equalTo:view.centerXAnchor).isActive = true
+        logo.centerYAnchor.constraint(equalTo:view.centerYAnchor).isActive = true
     }
     
-    private func loadResources() {
+    override func viewDidAppear(_ animated:Bool) {
         DispatchQueue.global(qos:.background).async {
             let _ = Factory.makeMaster()
             DispatchQueue.main.async {
