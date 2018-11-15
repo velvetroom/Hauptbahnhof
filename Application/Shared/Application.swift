d29b041f10b1bcab910be9cd4e38bb101182e293
vauxhall <zero.griffin@gmail.com>
2018-11-15 13:09:50 +0100

    Remove navigation

--- Application/Shared/Application.swift
+++ Application/Shared/Application.swift
@@ -2,7 +2,7 @@ import UIKit
 import Hauptbahnhof
 
 @UIApplicationMain class Application:UIResponder, UIApplicationDelegate {
-    static let navigation = Navigation()
+    static let navigation = UINavigationController()
     var window:UIWindow?
     
     func application(_:UIApplication, didFinishLaunchingWithOptions:[UIApplication.LaunchOptionsKey:Any]?) -> Bool {
@@ -16,6 +16,8 @@ import Hauptbahnhof
     }
     
     private func makeWindow() {
+        Application.navigation.setNavigationBarHidden(true, animated:false)
+        Application.navigation.setViewControllers([GameView()], animated:false)
         window = UIWindow(frame:UIScreen.main.bounds)
         window!.backgroundColor = .black
         window!.makeKeyAndVisible()
