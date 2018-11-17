8372cf46e7178c4a3611488a19c531fbd250d346
vauxhall <zero.griffin@gmail.com>
2018-11-17 10:04:43 +0100

    Add titles to chart

--- Application/Profile/ProfileView.swift
+++ Application/Profile/ProfileView.swift
@@ -42,6 +42,41 @@ class ProfileView:UIViewController {
         let chart = ProfileChartView()
         view.addSubview(chart)
         
+        let knowledge = UILabel()
+        knowledge.translatesAutoresizingMaskIntoConstraints = false
+        knowledge.isUserInteractionEnabled = false
+        knowledge.textAlignment = .center
+        knowledge.font = .systemFont(ofSize:12, weight:.ultraLight)
+        knowledge.textColor = .white
+        knowledge.text = .local("ProfileView.knowledge")
+        view.addSubview(knowledge)
+        
+        let courage = UILabel()
+        courage.translatesAutoresizingMaskIntoConstraints = false
+        courage.isUserInteractionEnabled = false
+        courage.textAlignment = .center
+        courage.font = .systemFont(ofSize:12, weight:.ultraLight)
+        courage.textColor = .white
+        courage.text = .local("ProfileView.courage")
+        view.addSubview(courage)
+        
+        let empathy = UILabel()
+        empathy.translatesAutoresizingMaskIntoConstraints = false
+        empathy.isUserInteractionEnabled = false
+        empathy.font = .systemFont(ofSize:12, weight:.ultraLight)
+        empathy.textColor = .white
+        empathy.text = .local("ProfileView.empathy")
+        view.addSubview(empathy)
+        
+        let diligence = UILabel()
+        diligence.translatesAutoresizingMaskIntoConstraints = false
+        diligence.isUserInteractionEnabled = false
+        diligence.font = .systemFont(ofSize:12, weight:.ultraLight)
+        diligence.textColor = .white
+        diligence.textAlignment = .right
+        diligence.text = .local("ProfileView.diligence")
+        view.addSubview(diligence)
+        
         let resume = ButtonTextView(.local("ProfileView.resume"), color:.spreeBlue)
         resume.addTarget(self, action:#selector(self.resume), for:.touchUpInside)
         view.addSubview(resume)
@@ -55,6 +90,18 @@ class ProfileView:UIViewController {
         chart.centerYAnchor.constraint(equalTo:view.centerYAnchor).isActive = true
         chart.widthAnchor.constraint(equalToConstant:200).isActive = true
         chart.heightAnchor.constraint(equalToConstant:200).isActive = true
+        
+        knowledge.bottomAnchor.constraint(equalTo:chart.topAnchor, constant:-6).isActive = true
+        knowledge.centerXAnchor.constraint(equalTo:chart.centerXAnchor).isActive = true
+        
+        courage.topAnchor.constraint(equalTo:chart.bottomAnchor, constant:6).isActive = true
+        courage.centerXAnchor.constraint(equalTo:chart.centerXAnchor).isActive = true
+        
+        empathy.leftAnchor.constraint(equalTo:chart.rightAnchor, constant:6).isActive = true
+        empathy.centerYAnchor.constraint(equalTo:chart.centerYAnchor).isActive = true
+        
+        diligence.rightAnchor.constraint(equalTo:chart.leftAnchor, constant:-6).isActive = true
+        diligence.centerYAnchor.constraint(equalTo:chart.centerYAnchor).isActive = true
         
         resume.centerXAnchor.constraint(equalTo:view.centerXAnchor).isActive = true
         
